69d1b225c3f7b18d10945adbe44f386d824b0d0d
Hasindu Udantha <hasinduudantha21@gmail.com>
2025-10-09 23:45:27 +0530

    modified:products

--- Grocery Store App/backend/products_DAO.py
+++ Grocery Store App/backend/products_DAO.py
@@ -1,1 +1,11 @@
 
+def get_all_products():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    conn.close()
+    return products
+
+            
+            

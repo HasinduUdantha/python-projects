e8f6dd56c17f7d81bf19cbc2349435958ae525a2
Hasindu Udantha <hasinduudantha21@gmail.com>
2025-10-11 22:46:43 +0530

    connect sql db

--- Grocery Store App/backend/products_DAO.py
+++ Grocery Store App/backend/products_DAO.py
@@ -1,11 +1,53 @@
+from sql_connection import get_sql_connection
 
-def get_all_products():
-    conn = sqlite3.connect('products.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM products")
-    products = cursor.fetchall()
-    conn.close()
-    return products
+def get_all_products(connection):
+    cursor = connection.cursor()
 
-            
-            
+    query = ("SELECT p.product_id, p.name, p.unitMessure_id, p.price_per_unit, u.uomName "
+             "FROM products p JOIN uom u ON p.unitMessure_id = u.unitMessure_id;")
+
+    cursor.execute(query)
+
+    response = []
+    
+    for (product_id, name, unitMessure_id, price_per_unit, uomName) in cursor:
+        response.append({
+            "product_id": product_id,
+            "name": name,
+            "unitMessure_id": unitMessure_id,
+            "price_per_unit": price_per_unit,
+            "uomName": uomName
+        })
+
+    return response
+
+def insert_new_product(connection, product):
+    cursor = connection.cursor()
+
+    query = ("INSERT INTO products (name, unitMessure_id, price_per_unit) "
+             "VALUES (%s, %s, %s)")
+    
+    data = (product['name'], product['unitMessure_id'], product['price_per_unit'])
+
+    cursor.execute(query, data)
+    connection.commit()
+    
+    return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+
+    query = ("DELETE FROM products WHERE product_id =" + str(product_id))
+    
+    cursor.execute(query)
+    connection.commit()
+    
+    return cursor.rowcount
+
+if __name__ == "__main__":
+    connection = get_sql_connection()
+    print(insert_new_product(connection, {
+    "name": "Banana",
+    "unitMessure_id": '1',
+    "price_per_unit": '120'
+    }))

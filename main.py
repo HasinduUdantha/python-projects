25cd877c0163e8ada7a681961f79db9e0785a953
Hasindu Udantha <hasinduudantha21@gmail.com>
2026-02-01 21:30:28 +0530

    add main.py with various function implementations including average calculation and a calculator

---
+++ main.py
@@ -0,0 +1,54 @@
+# # def calAvg(a,b,c):
+# #     avg = (a + b + c) / 3
+# #     print("Average is:", avg)
+    
+# # calAvg(10, 20, 30)        
+
+
+# def myFunc(*a):
+#     print(a)
+#     print("Type of a is:", type(a))
+#     b = sum(a)
+#     print("Sum is:", b)
+    
+#     c=b / len(a)
+#     print("Average is:", c)
+    
+# myFunc(10, 20, 30, 40, 50)    
+
+
+# def calculator(val1,val2):
+    
+#     add= val1 + val2
+#     sub= val1 - val2
+#     mul= val1 * val2
+#     div= val1 / val2
+    
+#     return add, sub, mul, div
+
+# val = calculator(100, 20)
+# print("Returned value is:", val)
+
+# a,b,c,d = calculator(10, 2)
+# print("Addition:", a)
+# print("Subtraction:", b)
+# print("Multiplication:", c)
+# print("Division:", d)
+
+
+
+# # Lambda function
+
+# # Syntax: lambda arguments: expression
+
+# def calAvg(a,b,c):
+#     avg = (a + b + c) / 3
+#     return avg
+
+# calAvgNew = lambda a,b,c: (a + b + c) / 3
+
+# val = calAvg(10, 20, 30)
+# print("Average using normal function is:", val)
+
+# valNew = calAvgNew(10, 20, 30)
+# print("Average using lambda function is:", valNew)
